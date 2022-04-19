ec3f9ccc1cd5775cf121b1b7d7a1f383787b863c
kathleendiep <kathleendiep@gmail.com>
2022-04-19 11:04:59 -0700

    banner

--- portfolio-react-diep/src/App.js
+++ portfolio-react-diep/src/App.js
@@ -9,23 +9,23 @@ import {
   Navigate,
 } from "react-router-dom";
 import 'animate.css';
+import scrollreveal from "scrollreveal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<Navigate replace to="/home" />} />
+        </Routes>
+      </Router>
+
+    <title>Kathleen Diep | Software Developer</title>
+    <meta name="keywords" content="kathleen diep, kathleen, react, javascript" />
+    <meta name="description" content="Kathleen Diep | Software Developer" />
+
     </div>
   );
 }
