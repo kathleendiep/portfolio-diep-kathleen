686b9756a7462d6b57965ee9ef5da166691ac73f
kathleendiep <kathleendiep@gmail.com>
2022-05-09 11:20:56 -0700

    added the icons responsive

--- portfolio-react-diep/src/components/aboutMe/aboutMe.jsx
+++ portfolio-react-diep/src/components/aboutMe/aboutMe.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
 import './aboutMe.scss'
+import { Icon } from '@iconify/react';
 
 const AboutMe = () => {
   return (
     <div class="well skewed-bg2 text-center">
       <div className="skewed-text">
 
-          <div className="about-img-full about-img">
-            <img src="/images/profilephoto.png" alt="Image" />
-          </div>
+        <div className="about-img-full about-img">
+          <img src="/images/profilephoto.png" alt="Image" />
+        </div>
 
         <div className="about-content">
           <h2>About Me</h2>
-          <h5 className="about-headline">software engineer |  entreprenuer  | sf bay area adventurer</h5> 
+          <h5 className="about-headline">software engineer |  entreprenuer  | sf bay area adventurer</h5>
           <p>Passionate about welding projects into existence starting from user experience then full stack technologies.
             With years of experience as a business owner, accountant, front-end web-developer, I have a strong foundation in driving result-oriented solutions.</p>
           <p>I have a passion to build knowledge, stay creative, and continually grow my mindset and I am motivated to leverage modern Front-end technologies to ship awesome user experiences.</p>
-          <div className="col-lg-4 col-sm-6">
-            <a className="default-btn card-container">
-              DOWNLOAD RESUME
-            </a>
-          </div>
-        </div>
 
+          <button className="default-btn card-container">
+            <a href="Kathleen-Diep-Resume-Software-Developer.pdf" download="KathleenDiepResume.pdf">
+              Download Resume <Icon icon="ep:download" />
+            </a>
+          </button>
+        </div>
       </div>
+
     </div>
 
   )
--- portfolio-react-diep/src/components/homePage/MainBanner.jsx
+++ portfolio-react-diep/src/components/homePage/MainBanner.jsx
@@ -22,22 +22,13 @@ const MainBanner = () => {
                         {/* Main Image */}
                         <div className="banner-main-img banner-one-main-img">
                             <img className="animate__animated animate__fadeInUp animate__fast" src="/images/aboutmeapple.png" alt="Image" />
-                            {/* <ul className="social-links">
-                                <a href="mailto:kathleendiep@gmail.com" className="email-link" target="_blank"><Icon icon="ic:baseline-email" /></a></li>
-                                <li><a href="https://github.com/kathleendiep/" target="_blank" class="github-link"><Icon icon="bytesize:github" /></a></li>
-                                <li><a href="https://www.linkedin.com/kathleen-diep/" target="_blank" class="linkedin-link"><Icon icon="brandico:linkedin-rect" /></a></li>
-                                <li><a href="https://twitter.com/imKathleenDiep" target="_blank" class="twitter-link"><Icon icon="brandico:twitter-bird" /></a></li>
-                            </ul> */}
-                           <a href="https://www.linkedin.com/kathleen-diep/" target="_blank" class="linkedin-link">Click here</a>
-                           <div class="links-social">
-                           <a href="https://www.linkedin.com/kathleen-diep/" target="_blank" class="linkedin-link">Click here</a>
+                        </div>
+                        <div className="links-social">
                                 <a href="mailto:kathleendiep@gmail.com" className="email-link" target="_blank"><Icon icon="ic:baseline-email" /></a>
                                 <a href="https://github.com/kathleendiep/" target="_blank" class="github-link"><Icon icon="bytesize:github" /></a>
-                               <a href="https://www.linkedin.com/kathleen-diep/" target="_blank" class="linkedin-link"><Icon icon="brandico:linkedin-rect" /></a>
-                               <a href="https://twitter.com/imKathleenDiep" target="_blank" class="twitter-link"><Icon icon="brandico:twitter-bird" /></a>
-                            </div> 
-             
-                        </div>
+                                <a href="https://www.linkedin.com/kathleen-diep/" target="_blank" class="linkedin-link"><Icon icon="brandico:linkedin-rect" /></a>
+                                <a href="https://twitter.com/imKathleenDiep" target="_blank" class="twitter-link"><Icon icon="brandico:twitter-bird" /></a>
+                        </div> 
 
                     </div>
                 </div>
