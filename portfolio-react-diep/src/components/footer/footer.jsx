96d219a5adac1c861414349f0ff94dfed832fdde
kathleendiep <kathleendiep@gmail.com>
2022-05-10 14:27:09 -0700

    responsive banner

--- portfolio-react-diep/src/components/footer/footer.jsx
+++ portfolio-react-diep/src/components/footer/footer.jsx
@@ -12,9 +12,8 @@ const Footer = () => {
                         <li className="social-icons"> <a href="https://github.com/kathleendiep/" target="_blank" class="github-link"><Icon icon="bytesize:github" /></a></li> 
                         <li className="social-icons"><a href="https://www.linkedin.com/kathleen-diep/" target="_blank" class="linkedin-link"><Icon icon="brandico:linkedin-rect" /></a></li>
                         <li className="social-icons"><a href="https://twitter.com/imKathleenDiep" target="_blank" class="twitter-link"><Icon icon="brandico:twitter-bird" /></a></li>
-
+                        <li>    &copy;2022 Kathleen Diep | All Rights Reserved </li>
                     </ul>
-                    &copy;2022 Kathleen Diep | All Rights Reserved
                 </div>
         </footer> 
     )
--- portfolio-react-diep/src/components/homePage/MainBanner.jsx
+++ portfolio-react-diep/src/components/homePage/MainBanner.jsx
@@ -12,13 +12,13 @@ const MainBanner = () => {
         <section className="main-banner-area">
             <div className="container-fluid">
                 <div className="row align-items-center banner-container">
-                    <div className="col-lg-6 ">
-                        <div className="banner-text">
+                    <div className="col-lg-6 banner-text">
+                        {/* <div className="banner-text"> */}
                             <h1 className="gradient">Kathleen Diep</h1>
                             <p>Full Stack Web Engineer</p>
-                        </div>
+                        {/* </div> */}
                     </div>
-                    <div className="col-lg-6 banner-one-main-img">
+                    <div className="col-lg-6">
                         {/* Main Image */}
                         <div className="banner-main-img">
                             <img className="animate__animated animate__fadeInUp animate__fast" src="/images/aboutmeapple.png" alt="Image" />
@@ -37,7 +37,6 @@ const MainBanner = () => {
                                 <li className="social-icons"> <a href="https://github.com/kathleendiep/" target="_blank" class="github-link"><Icon icon="bytesize:github" /></a></li> 
                                 <li className="social-icons"><a href="https://www.linkedin.com/kathleen-diep/" target="_blank" class="linkedin-link"><Icon icon="brandico:linkedin-rect" /></a></li>
                                 <li className="social-icons"><a href="https://twitter.com/imKathleenDiep" target="_blank" class="twitter-link"><Icon icon="brandico:twitter-bird" /></a></li>
-
                             </ul>
                         </div>
                     </div>
